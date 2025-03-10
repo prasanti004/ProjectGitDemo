5209b2b6b943a919f1cdde124deb3d75706cf134
madhavi004 <madhavinadiminti004@gmail.com>
2025-03-10 16:16:30 +0530

    variable declaration

--- test2.py
+++ test2.py
@@ -1,6 +1,12 @@
-#x ,y ,z = "Orange", "Banana", "Cherry"
-#print(x)
+x ,y ,z = "Orange", "Banana", "Cherry"
+print(x)
 
 fruits = ['apple', 'banana', 'cherry']
 a, b, c = fruits
 print(a)
+
+print('Hello','World')
+
+a = 'Hello'
+b = 'World'
+print(a + b)
