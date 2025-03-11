abc8536bcff38c982114e044d1e166a0b87d0f2a
madhavi004 <madhavinadiminti004@gmail.com>
2025-03-11 20:15:22 +0530

    while loop

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+#x=5
+#print(type(x))
+
+#x = list(("apple", "banana", "cherry"))
+#print(x)
+
+my_tuple=(1,2,3,4,5)
+print(my_tuple[1])
+
+x = tuple(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
---
+++ whileloop.py
@@ -0,0 +1,4 @@
+x = 4
+while x<6:
+    print(x)
+    x+=1
